bfb8909ce308788432b407155e975161ccd754bc
DC <149849984+gojohatake456@users.noreply.github.com>
2025-02-15 17:38:13 +0530

    Your commit message

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import Message  from './Message';
+import Message from './Message';
+import ListGroup from "./Components/ListGroup";
 
 function App() {
-  return <div>
-    <Message></Message> 
+  return (
+    <div>
+      <Message />
+      <ListGroup />
     </div>
+  );
 }
-// or self closing syntax  brac messge / brac
+
 export default App;
+
+
+
+
+//or self closing syntax  brac messge / brac
+//export default App;
+
+
+
+
+    
+
+// or self closing syntax  brac messge / brac
